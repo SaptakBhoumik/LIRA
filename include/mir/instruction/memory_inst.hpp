8f20bec0f8e02ff85a9b62c1c505b67bb8375917
SaptakBhoumik <saptakbhoumik@gmail.com>
2026-06-06 22:55:11 +0530

    .

--- include/mir/instruction/memory_inst.hpp
+++ include/mir/instruction/memory_inst.hpp
@@ -30,7 +30,7 @@ class AllocaInst:public Inst {
     IR::LiteralExprPtr size_var;//The size variable 
 
     std::size_t alignment;//The alignment of the allocated memory. Must be a power of 2 and less than or equal to 16. We need this because we want to support alloca with alignment in future. For now we can just set it to 0 and ignore it.
-     public:
+    public:
     AllocaInst(IR::InstructionStmtPtr instruction_stmt, std::optional<DestinationVar> destination, IR::TypeExprPtr size_var_type, 
                 IR::LiteralExprPtr size_var, std::size_t alignment);
 
@@ -45,6 +45,7 @@ class AllocaInst:public Inst {
 };
 
 class LoadInst:public Inst {
+    protected:
     IR::InstructionStmtPtr instruction_stmt;
     std::optional<DestinationVar> destination;//The destination variable token for identifying the output variable
 
@@ -52,7 +53,7 @@ class LoadInst:public Inst {
     IR::TypeExprPtr type;//The reduced type of the output
 
     bool volatile_ = false;//Whether it has the volatile attribute or not
-    bool temporal = false;//Whether it has the temporal attribute or not
+    bool nontemporal = false;//Whether it has the nontemporal attribute or not
     bool invariant_load = false;//Whether it has the invariant.load attribute or not
     bool nonull = false;
     bool nopoison = false;
@@ -60,44 +61,162 @@ class LoadInst:public Inst {
     std::size_t dereferenceable_bytes; //If 0 means the value is not set
     public:
     LoadInst(IR::InstructionStmtPtr instruction_stmt, std::optional<DestinationVar> destination, IR::LiteralExprPtr pointer, IR::TypeExprPtr type, 
-             bool volatile_, bool temporal, bool invariant_load, bool nonull, bool nopoison, std::size_t alignment, std::size_t dereferenceable_bytes);
+             bool volatile_, bool nontemporal, bool invariant_load, bool nonull, bool nopoison, std::size_t alignment, std::size_t dereferenceable_bytes);
 
     virtual ~LoadInst() = default;
 
     virtual IR::LiteralExprPtr get_pointer() const final;
     virtual IR::TypeExprPtr get_type() const final;
     virtual bool is_volatile() const final;
-    virtual bool is_temporal() const final;
+    virtual bool is_nontemporal() const final;
     virtual bool is_invariant_load() const final;
     virtual bool is_nonull() const final;
     virtual bool is_nopoison() const final;
     virtual std::size_t get_alignment() const final;
     virtual std::size_t get_dereferenceable_bytes() const final;
 
-    virtual std::optional<std::pair<DestinationVar,IR::TypeExprPtr>> get_destination() const override;//The type of destination is type
+    virtual std::optional<std::pair<DestinationVar,IR::TypeExprPtr>> get_destination() const override final;//The type of destination is type
     virtual InstType get_inst_type() const override;
-    virtual IR::InstructionStmtPtr get_instruction_stmt() const override;
+    virtual IR::InstructionStmtPtr get_instruction_stmt() const override final;
     virtual std::string to_string() const override;
 };
 
 class AtomicLoadInst: public LoadInst{
+    protected:
     SyncScope sync_scope;
     AtomicOrdering ordering;
 
     public:
     AtomicLoadInst(IR::InstructionStmtPtr instruction_stmt, std::optional<DestinationVar> destination, IR::LiteralExprPtr pointer, IR::TypeExprPtr type, 
-                    bool volatile_, bool temporal, bool invariant_load, bool nonull, bool nopoison, std::size_t alignment, std::size_t dereferenceable_bytes, 
+                    bool volatile_, bool nontemporal, bool invariant_load, bool nonull, bool nopoison, std::size_t alignment, std::size_t dereferenceable_bytes, 
                     SyncScope sync_scope, AtomicOrdering ordering);
 
     SyncScope get_sync_scope() const;
     AtomicOrdering get_ordering() const;
 
-    std::optional<std::pair<DestinationVar,IR::TypeExprPtr>> get_destination() const override;//The type of destination is type
+    InstType get_inst_type() const override;
+    std::string to_string() const override;
+};
+class StoreInst:public Inst {
+    protected:
+    IR::InstructionStmtPtr instruction_stmt;
+
+    IR::LiteralExprPtr pointer;//The pointer to store to. Must be of type ptr
+    IR::LiteralExprPtr value;//The value to store.
+    IR::TypeExprPtr type;//The reduced type of the value. 
+
+    bool volatile_ = false;//Whether it has the volatile attribute or not
+    bool nontemporal = false;//Whether it has the nontemporal attribute or not
+    bool nonull = false;
+    bool nopoison = false;
+    std::size_t alignment;
+    std::size_t dereferenceable_bytes; //If 0 means the value is not set
+    public:
+    StoreInst(IR::InstructionStmtPtr instruction_stmt, IR::LiteralExprPtr pointer, IR::LiteralExprPtr value, IR::TypeExprPtr type, 
+              bool volatile_, bool nontemporal, bool nonull, bool nopoison, std::size_t alignment, std::size_t dereferenceable_bytes);
+
+    virtual ~StoreInst() = default;
+
+    virtual IR::LiteralExprPtr get_pointer() const final;
+    virtual IR::LiteralExprPtr get_value() const final;
+    virtual IR::TypeExprPtr get_type() const final;
+    virtual bool is_volatile() const final;
+    virtual bool is_nontemporal() const final;
+    virtual bool is_nonull() const final;
+    virtual bool is_nopoison() const final;
+    virtual std::size_t get_alignment() const final;
+    virtual std::size_t get_dereferenceable_bytes() const final;
+
+    virtual std::optional<std::pair<DestinationVar,IR::TypeExprPtr>> get_destination() const override final;//Store instruction does not have a destination variable
+    virtual InstType get_inst_type() const override;
+    virtual IR::InstructionStmtPtr get_instruction_stmt() const override final;
+    virtual std::string to_string() const override;
+};
+
+class AtomicStoreInst: public StoreInst{
+    protected:
+    SyncScope sync_scope;
+    AtomicOrdering ordering;
+
+    public:
+    AtomicStoreInst(IR::InstructionStmtPtr instruction_stmt, IR::LiteralExprPtr pointer, IR::LiteralExprPtr value, IR::TypeExprPtr type, 
+                    bool volatile_, bool nontemporal, bool nonull, bool nopoison, std::size_t alignment, std::size_t dereferenceable_bytes, 
+                    SyncScope sync_scope, AtomicOrdering ordering);
+
+    SyncScope get_sync_scope() const;
+    AtomicOrdering get_ordering() const;
+
+    InstType get_inst_type() const override;
+    std::string to_string() const override;
+};
+
+class GetAddressInst:public Inst {
+    IR::InstructionStmtPtr instruction_stmt;
+    std::optional<DestinationVar> destination;//The destination variable token for identifying the output variable
+
+    IR::LiteralExprPtr value;//The value to get the address of.
+    IR::TypeExprPtr value_type;//The reduced type of the value.
+    IR::LiteralExprPtr offset;//The offset in bytes to add to the address. You get &value + offset and not &value[offset]
+    IR::TypeExprPtr offset_type;//The type of the offset. Must be of integer type
+
+    bool signed_;
+    bool nsw;
+    bool nuw;
+    bool inbounds;
+    public:
+    GetAddressInst(IR::InstructionStmtPtr instruction_stmt, std::optional<DestinationVar> destination, IR::LiteralExprPtr value, IR::TypeExprPtr value_type,
+                    IR::LiteralExprPtr offset, IR::TypeExprPtr offset_type, bool signed_, bool nsw, bool nuw, bool inbounds);
+
+    IR::LiteralExprPtr get_value() const;
+    IR::TypeExprPtr get_value_type() const;
+    IR::LiteralExprPtr get_offset() const;
+    IR::TypeExprPtr get_offset_type() const;
+    std::shared_ptr<IR::IntTypeExpr> get_offset_int_type() const;//Returns the type of offset casted to IntTypeExpr. Just a helper function to make life easier
+    std::size_t get_offset_bit_width() const;//Returns the bit width of the offset type. Just a helper function to make life easier
+
+    bool is_signed() const;
+    bool is_nsw() const;
+    bool is_nuw() const;
+    bool is_inbounds() const;
+
+    std::optional<std::pair<DestinationVar,IR::TypeExprPtr>> get_destination() const override;//Output type is ptr
     InstType get_inst_type() const override;
     IR::InstructionStmtPtr get_instruction_stmt() const override;
     std::string to_string() const override;
 };
-//TODO:Like these define the other instruciton that have a atomic varient like store, atomic store etc
+
+class PtrOffsetInst:public Inst {
+    IR::InstructionStmtPtr instruction_stmt;
+    std::optional<DestinationVar> destination;//The destination variable token for identifying the output variable
+
+    IR::LiteralExprPtr pointer;//The pointer to add the offset to. Must be of type ptr
+    IR::LiteralExprPtr offset;//The offset in bytes to add to the pointer. You get pointer + offset and not pointer[offset]
+    IR::TypeExprPtr offset_type;//The type of the offset. Must be of integer type
+
+    bool signed_;
+    bool nsw;
+    bool nuw;
+    bool inbounds;
+    public:
+    PtrOffsetInst(IR::InstructionStmtPtr instruction_stmt, std::optional<DestinationVar> destination, IR::LiteralExprPtr pointer, 
+                    IR::LiteralExprPtr offset, IR::TypeExprPtr offset_type, bool signed_, bool nsw, bool nuw, bool inbounds);
+
+    IR::LiteralExprPtr get_pointer() const;
+    IR::LiteralExprPtr get_offset() const;
+    IR::TypeExprPtr get_offset_type() const;
+    std::shared_ptr<IR::IntTypeExpr> get_offset_int_type() const;//Returns the type of offset casted to IntTypeExpr. Just a helper function to make life easier
+    std::size_t get_offset_bit_width() const;//Returns the bit width of the offset type. Just a helper function to make life easier
+
+    bool is_signed() const;
+    bool is_nsw() const;
+    bool is_nuw() const;
+    bool is_inbounds() const;
+
+    std::optional<std::pair<DestinationVar,IR::TypeExprPtr>> get_destination() const override;//Output type is ptr
+    InstType get_inst_type() const override;
+    IR::InstructionStmtPtr get_instruction_stmt() const override;
+    std::string to_string() const override;
+};
 class ExtractElementInst:public Inst {
     protected:
     IR::InstructionStmtPtr instruction_stmt;
@@ -156,6 +275,60 @@ class InsertElementInst:public Inst {
     std::string to_string() const override;
 };
 
+class FenceInst:public Inst {
+    IR::InstructionStmtPtr instruction_stmt;
+    std::optional<DestinationVar> destination;//The destination variable token for identifying the output variable
 
+    SyncScope sync_scope;
+    AtomicOrdering ordering;
+    public:
+    FenceInst(IR::InstructionStmtPtr instruction_stmt, std::optional<DestinationVar> destination, SyncScope sync_scope, AtomicOrdering ordering);
+
+    SyncScope get_sync_scope() const;
+    AtomicOrdering get_ordering() const;
+
+    std::optional<std::pair<DestinationVar,IR::TypeExprPtr>> get_destination() const override;//Fence instruction does not have a destination variable
+    InstType get_inst_type() const override;
+    IR::InstructionStmtPtr get_instruction_stmt() const override;
+    std::string to_string() const override;
+};
+
+
+class CmpXchgInst: public Inst {
+    IR::InstructionStmtPtr instruction_stmt;
+    std::optional<DestinationVar> destination;//The destination variable token for identifying the output variable
+
+    IR::LiteralExprPtr pointer;//The pointer to compare and exchange. Must be of type ptr
+    IR::LiteralExprPtr expected;//The expected value. The type must be same as the reduced type of pointer
+    IR::LiteralExprPtr desired;//The desired value. The type must be same as the reduced type of pointer
+    IR::TypeExprPtr type;//The reduced type of expected and desired. Output type is {type,i1}
+
+    SyncScope sync_scope;
+    AtomicOrdering success_ordering;
+    AtomicOrdering failure_ordering;
+    std::size_t alignment;
+    bool volatile_ = false;//Whether it has the volatile attribute or not
+    bool weak = false;//Whether it has the weak attribute or not. 
+    public:
+    CmpXchgInst(IR::InstructionStmtPtr instruction_stmt, std::optional<DestinationVar> destination, IR::LiteralExprPtr pointer, IR::LiteralExprPtr expected, 
+                IR::LiteralExprPtr desired, IR::TypeExprPtr type, SyncScope sync_scope, AtomicOrdering success_ordering, AtomicOrdering failure_ordering, 
+                std::size_t alignment, bool volatile_, bool weak);
+
+    IR::LiteralExprPtr get_pointer() const;
+    IR::LiteralExprPtr get_expected() const;
+    IR::LiteralExprPtr get_desired() const;
+    IR::TypeExprPtr get_type() const;
+    SyncScope get_sync_scope() const;
+    AtomicOrdering get_success_ordering() const;
+    AtomicOrdering get_failure_ordering() const;
+    std::size_t get_alignment() const;
+    bool is_volatile() const;
+    bool is_weak() const;
+
+    std::optional<std::pair<DestinationVar,IR::TypeExprPtr>> get_destination() const override;//The type of destination is a struct containing the old value and a boolean indicating whether the exchange was successful or not. We can represent this as a struct with two fields: {old_value, success}. The old_value field has the same type as the reduced type of pointer and the success field is of type i1.
+    InstType get_inst_type() const override;
+    IR::InstructionStmtPtr get_instruction_stmt() const override;
+    std::string to_string() const override;
+};
 }
 }
